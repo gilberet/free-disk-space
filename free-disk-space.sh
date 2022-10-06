a262ddcd5038e21fdbe946ccdec13277b4c7479d
gilberet <gilberet@hotmail.com>
2022-10-06 15:09:06 -0400

    Se agrego comillas.

--- free-disk-space.sh
+++ free-disk-space.sh
@@ -5,5 +5,5 @@ find /u -iname "*.xls" >/u/dev-mc/free-disk-space/$(date +%d%m%Y)_file_xls.txt
 while IFS= read -r line; do
   IFS='/'
   read -a strarr <<<"$line"
-  rm -f $line
+  rm -f "$line"
 done </u/dev-mc/free-disk-space/$(date +%d%m%Y)_file_xls.txt
