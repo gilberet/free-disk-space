6c89549c1db2d19cb4a2736135082181e10d478a
gilberet <gilberet@hotmail.com>
2022-10-06 14:13:18 -0400

    Se quito el codigo de ejemplo

--- free-disk-space.sh
+++ free-disk-space.sh
@@ -1,14 +1,9 @@
 #! /bin/bash
 
-find /u  -iname "*.xls"  > /u/dev-mc/free-disk-space/$(date +%d%m%Y)_file_xls.txt
+find /u -iname "*.xls" >/u/dev-mc/free-disk-space/$(date +%d%m%Y)_file_xls.txt
 
-while IFS= read -r line
-do
+while IFS= read -r line; do
   IFS='/'
-  read -a strarr <<< "$line"
-  #echo "$line"
-  #cp "$line" /u/dev-mc/${strarr[2]}.xls
-  rm -f /u/dev-mc/${strarr[2]}.xls
-done < /u/dev-mc/free-disk-space/$(date +%d%m%Y)_file_xls.txt
-
-
+  read -a strarr <<<"$line"
+  rm -f $line
+done </u/dev-mc/free-disk-space/$(date +%d%m%Y)_file_xls.txt
